13f38059f811b35215fdc8570bc2242365264884
Raquel Ruiz <raquelruizlopezcepero@gmail.com>
2025-10-16 11:29:15 +0200

    feature(dashboard): add DashboardHome page

---
+++ src/dashboard/pages/DashboardHome.jsx
@@ -0,0 +1,11 @@
+export const DashboardHome = () => {
+	return (
+		<section>
+			<h1 className="text-3xl font-bold text-primary mb-4">Panel de control</h1>
+			<p className="text-gray-600">
+				Bienvenido al panel de administración de <strong>Woodline Living</strong>.
+                Aquí podras gestionar productos, reseñas y usuarios.
+			</p>
+		</section>
+	);
+};
