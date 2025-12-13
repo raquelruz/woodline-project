a39f83b0c456004669649c7896154c5a7993c9db
Raquel Ruiz <raquelruizlopezcepero@gmail.com>
2025-12-13 19:39:34 +0100

    refactor(profile): modularize Profile component into hooks + UI components

--- src/landing/components/SectionContainer.jsx
+++ src/landing/components/SectionContainer.jsx
@@ -1,4 +1,3 @@
-import { memo } from "react";
 import { Container } from "./Container";
 
 export const SectionContainer = ({ bg, title, childrenContainerStyle, children }) => {
---
+++ src/landing/sections/FavoritesSection.jsx
@@ -0,0 +1,20 @@
+import { EmptyFavorites } from "../components/Favorites/EmptyFavorites";
+import { FavoritesCard } from "../components/Favorites//FavoritesCard"
+
+export const FavoritesSection = ({ favorites }) => {
+    const hasFavorites = favorites.length > 0;
+
+    return (
+        <div className="p-4">
+            <h2 className="text-3xl text-primary font-semibold mb-6">
+                Productos favoritos ♥
+            </h2>
+
+            {!hasFavorites ? (
+                <EmptyFavorites />
+            ) : (
+                <FavoritesCard products={favorites} />
+            )}
+        </div>
+    );
+};
